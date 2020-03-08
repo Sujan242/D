a0614565a10b05ed9f90654e48ed3a66a45e3b7c
Shashi181096 <shashikanthgk@gmail.com>
2020-03-09 01:11:47 +0530

    commited

---
+++ spanningtrees/DPV5.7.py
@@ -0,0 +1,103 @@
+class Node:
+    def __init__(self,x):
+        self.value = x
+        self.parent = self
+        self.height = 0
+
+    def __str__(self):
+        return str(self.value)
+
+    
+
+class DisjointSets: 
+
+
+    def getnode(self,value):
+        x = Node(value)
+        return x
+
+
+        
+    def makeset(self,value):
+        return self.getnode(value)
+
+    def findset(self,x):
+        if(x.parent == x):
+            return x
+        x.parent = self.findset(x.parent)
+        return x.parent
+
+    def union(self,x,y):
+        px = self.findset(x)
+        py = self.findset(y)
+        if(px==py):
+            return
+        if(px.height>py.height):
+            py.parent = px
+        elif(px.height<py.height):
+            px.parent = py
+        else:
+            px.parent = py
+            py.height = py.height+1
+
+
+
+
+
+
+def kruskal(edges,vertexs,x):
+    edges = sorted(edges,key = lambda x:x[1],reverse=True)
+    T = set()
+    sum = 0
+    for i in range(len(edges)):
+        if(x.findset(vertexs[edges[i][0][0]])!=x.findset(vertexs[edges[i][0][1]])):
+            T.add(edges[i][0])
+            sum = sum+edges[i][1]
+            x.union(vertexs[edges[i][0][0]],vertexs[edges[i][0][1]])
+    print(T,sum)
+
+
+    
+    
+
+
+
+def take_input():
+    Graph = []
+    k_graph  = []
+    file = open('input3.txt','r')
+    k = 0
+    for line in file:
+        first = True
+        adjacentvertices = []
+        for node in line.split(' '):
+            if first:
+                first =False
+                continue
+            edge,weight = node.split(',') 
+            adjacentvertices.append((int(edge),int(weight)))
+            k_graph.append(((k,int(edge)),int(weight)))
+        k += 1
+        Graph.append(adjacentvertices)
+    # print(Graph)
+    print(k_graph)
+    x = DisjointSets()
+    vertexs= []
+    for i in range(len(Graph)):
+        vertexs.append(x.makeset(i))
+    kruskal(k_graph,vertexs,x)
+
+
+        
+
+
+def main():
+    take_input()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
